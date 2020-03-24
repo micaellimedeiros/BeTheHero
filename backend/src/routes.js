0108dcb06602bd419fad09c626d48e1d9c80f848
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-24 20:04:42 -0300

    route file

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    evento: 'Semana Omnistack 11.0',
-    aluno: 'Micaelli Medeiros'
-  })
-});
+routes.post('/sessions', SessionController.create)
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
 
 module.exports = routes;
