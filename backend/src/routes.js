1b7ea4f2c3066782e295b144f90bbeb8a44c4f91
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-27 13:37:49 -0300

    route updated with authentication

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -16,7 +16,7 @@ routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
     email: Joi.string().required().email(),
-    whatsapp: Joi.number().required().min(10).max(11),
+    whatsapp: Joi.string().required().min(10).max(14),
     city: Joi.string().required(),
     uf: Joi.string().required().length(2),
   })
