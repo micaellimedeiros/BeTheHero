4b350735c11ee699a5e32cac5f12f93c562340ff
Micaelli Medeiros <m.amedeiros@hotmail.com>
2020-03-27 13:35:03 -0300

    app file

---
+++ backend/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
